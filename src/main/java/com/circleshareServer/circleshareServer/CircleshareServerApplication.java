13889197f084d45dfd62ed380d6a2d954c645c76
sai <sairamreddy2211@gmail.com>
2024-11-26 22:42:20 +0530

    initail commit

---
+++ src/main/java/com/circleshareServer/circleshareServer/CircleshareServerApplication.java
@@ -0,0 +1,13 @@
+package com.circleshareServer.circleshareServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleshareServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleshareServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/circleshareServer/circleshareServer/CircleshareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circleshareServer.circleshareServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleshareServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
